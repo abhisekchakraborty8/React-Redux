a6550d9700f9eda1f74eb1238faee6f72e31abe5
Abhisek Chakraborty <abhisekchakraborty8@gmail.com>
2018-10-14 21:20:56 +1100

    React Router n Single page app

--- src/components/DishDetailComponent_needchange.js
+++ src/components/DishDetailComponent_needchange.js
@@ -20,21 +20,28 @@ import {Link} from 'react-router-dom';
             );
     }
 
+    function RenderComments({comments}) {
+        if(comments != null)
+            return(
+
+                {comments.map((comment) => {
+                    return(
+                        <li key={comment.id}>
+                        <p>{comment.comment}</p>
+                        <p>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))} </p>
+                        </li>    
+                        );
+                })}
+            
+            );
+        else
+            return(
+                <div></div>
+                );
+
+        }
 
     const DishDetail = (props) =>{
-
-    	if(props.comments !=null){
-    	const dishC = props.comments.map((comments) => {
-        return (
-        		<div key={comments.id}>
-                      <CardBody>
-                      	<CardText>{comments.comment} </CardText>
-                      	<CardText>--{comments.author}, {new Intl.DateTimeFormat('en-US', 
-                      		{ year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comments.date)))} </CardText>
-                      </CardBody>
-               </div>
-            );
-        });
     
     return(
     	<div className="container">
@@ -54,18 +61,16 @@ import {Link} from 'react-router-dom';
             		<RenderDish dish= {props.dish} />
             	</div>
             	<div  className="col-12 col-md-5 m-1">
-            		
-            			<CardTitle>Comments</CardTitle>
-            				{dishC}
-            		
+            	<h4>Comments</h4>
+                <ul className="list-unstyled">
+            		<RenderComments comments={props.comments} />
+            	</ul>	
             	</div>
             	
     		</div>
     	</div>
     	);
-} else {
-	return(<div></div>);
-	}
+
 }
 
 export default DishDetail;
